88bb13977cfbdc5dcbc89ba9821fb0bbf498c553
olamijinadebayo <consultolamijin@gmail.com>
2018-02-23 17:04:34 +0300

    this will add the layout for the page and print the numbers to the console

---
+++ js/script.js
@@ -0,0 +1,34 @@
+var inp;
+var count = function () {
+
+  var s = [];
+  for (var i = 1; i <= inp; i++) {
+    // s.push(3);
+    if (i % 3 === 0) {
+      s.push("ping");
+    } else if (i % 5 === 0) {
+      s.push("pong");
+    } else if (i % 3 === 0) && i % 5 === 0) {
+    S.push("ping pong");
+  } else {
+    s.push(i);
+  }
+  // if (i < (inp)) {
+  //   s += ',';
+  // }
+  // s += i;
+  // s.push(i++);
+}
+return s;
+};
+$(document).ready(function () {
+  $("#enter form").submit(function (e) {
+    e.preventDefault();
+    inp = parseInt($("#info").val());
+
+    var input = count(inp);
+    console.log(input);
+    // var darray = [];
+
+  });
+});
