b134b851daeb5e8c7153b2daa0a9e9ecb5f181ae
olamijinadebayo <consultolamijin@gmail.com>
2018-02-25 06:37:43 +0300

    This will modify the business logic
    this will add the divisibility rules guiding the program

--- js/script.js
+++ js/script.js
@@ -4,22 +4,22 @@ var count = function () {
   var s = [];
   for (var i = 1; i <= inp; i++) {
     // s.push(3);
-    if (i % 3 === 0) {
+    if ((i % 3 === 0) && (i % 5 === 0)) {
+      s.push("ping pong");
+    } else if (i % 3 === 0) {
       s.push("ping");
     } else if (i % 5 === 0) {
       s.push("pong");
-    } else if (i % 3 === 0) && i % 5 === 0) {
-    S.push("ping pong");
-  } else {
-    s.push(i);
+    } else {
+      s.push(i);
+    }
+    // if (i < (inp)) {
+    //   s += ',';
+    // }
+    // s += i;
+    // s.push(i++);
   }
-  // if (i < (inp)) {
-  //   s += ',';
-  // }
-  // s += i;
-  // s.push(i++);
-}
-return s;
+  return s;
 };
 $(document).ready(function () {
   $("#enter form").submit(function (e) {
